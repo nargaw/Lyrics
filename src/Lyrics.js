9d3e3eab42cb8bb5c269234daedafd60f4fc6f20
Nate Argaw <59675817+nargaw@users.noreply.github.com>
2023-01-18 16:55:02 -0500

    update display

--- src/Lyrics.js
+++ src/Lyrics.js
@@ -20,36 +20,20 @@ export default function Lyrics()
         getLyrics()
     }, [])
 
-    
-    
-
-    const textRef = useRef()
-    if(textRef)
-    {
-        // textRef.material.wireframe = true
-        console.log(textRef.current)
-    }
     let line = ''
-    console.log(lyrics)
     if(lyrics.lyrics){
-        const firstLine = lyrics.lyrics[0].split(" ")
-        console.log(firstLine)
         line = lyrics.lyrics[0].split(" ")
     }
-    const twoWord = ['Banana', 'Monkey']
-    const testWord = 'Word'
-    const testArray = ['this', 'is', 'a', 'test', 'text']
-    const testText = 'This is a test text'
-    console.log(line)
-    
-    let previousBoundingBox = null
+
+    const textRef = useRef()
+    console.log(textRef.current)
 
     const DisplayLyrics = () => 
     {
         return <>
             {[...Array(line.length)].map((value,index) =>
                 <RigidBody
-                    ref={textRef}
+                   
                     key={index}
                     position={[
                         index - 2.5,
@@ -60,23 +44,20 @@ export default function Lyrics()
                     <Center>
                         <Text3D
                             name={index}
-                            
+                            ref={textRef}
                             key={index} 
                             font={'./Roboto.json'}
                             size={0.75}
                         >
                                 {line[index]}
-                                {console.log(textRef.position)}
                         </Text3D>
                     </Center>
                 </RigidBody>
             )}
         </>
-        
     }
     
     return <>
-        {lyricsStatus === 'loaded' ? <DisplayLyrics /> : console.log('no')}
-        
+        {lyricsStatus === 'loaded' && <DisplayLyrics />}
     </>
 }
