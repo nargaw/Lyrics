11ce2276fd87d116c2a3c8d5891d317b14de332f
Nate Argaw <59675817+nargaw@users.noreply.github.com>
2023-01-23 12:21:01 -0500

    update lyrics and interface

--- src/Lyrics.js
+++ src/Lyrics.js
@@ -1,8 +1,10 @@
 import { RigidBody } from "@react-three/rapier"
 import { Text3D, Center } from "@react-three/drei"
 import useLyrics from "./stores/useLyrics.js"
-import { useFrame } from "@react-three/fiber"
+import { useFrame, addEffect } from "@react-three/fiber"
 import * as THREE from 'three'
+import { useEffect } from "react"
+
 import { useRef, forwardRef } from "react"
 
 export default function Lyrics()
@@ -14,7 +16,52 @@ export default function Lyrics()
     })
 
     const lyrics = useLyrics(state => state.getLyrics())
-    const time = useLyrics(state => state.timer)
+    // const time = useLyrics(state => state.timer)
+    const songStatus = useLyrics(state => state.songStatus)
+
+
+
+
+    const time = useEffect(() =>
+    {
+        const unsubscribeEffect = addEffect(() =>
+        {   
+            // // const element = document.querySelector('.progress')
+            // // console.log(element.style.width)
+            // let elapsedTime = 0
+            // // element.style.width = ((elapsedTime/241) * 100) + '%'
+            // const state = useLyrics.getState()
+            // const startTime = getSongTime()
+            // // console.log(startTime)
+            // if(songStatus === "playing")
+            // {
+            //     elapsedTime = 0   
+            // }
+
+            // elapsedTime = Date.now() - startTime
+            // elapsedTime /= 1000
+            // // if(element.style.width && elapsedTime <= 241)
+            // // {
+            // //     console.log((elapsedTime/241) * 100)
+            // //     element.style.width = ((elapsedTime/241) * 100) + '%'
+            // // }
+            // // console.log(songStatus)
+            // if(time.current && elapsedTime > 0 && elapsedTime <= 242){
+            //     time.current.textContent = timeDisplay(elapsedTime)
+            //     // element.style.width = ((elapsedTime/241) * 100) + '%'
+            //     // console.log(time.current)
+            //     progress.current.style.width = ((elapsedTime/241) * 100) + '%'
+            //     // setTimer(elapsedTime) 
+            // }   
+        })
+
+        return () =>
+        {
+            unsubscribeEffect()
+        }
+    }, [])
+
+
     // console.log(time)
     const DisplayLyrics = ({num}) => 
     {
@@ -52,11 +99,11 @@ export default function Lyrics()
     }
     
     return <>
-        {time > 0 && time < 20 && [...Array(5)].map((value, index) => 
+        {/* {[...Array(5)].map((value, index) => 
             <DisplayLyrics num={index} key={index} />
         )}
-        {time >= 20 && time < 40 && [...Array(5)].map((value, index) => 
+        {[...Array(5)].map((value, index) => 
             <DisplayLyrics num={index + 5} key={index + 5}/>
-        )}
+        )} */}
     </>
 }
